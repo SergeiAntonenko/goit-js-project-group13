380944af61857ca33602fb195e6f76a782a3af60
Dmytro Kasko <dimoks77@gmail.com>
2024-03-03 01:57:35 +0100

    Ночной коммит
    Сделал кнопку All Categories под книгами.
    Исправил слушатели событий

--- src/js/home.js
+++ src/js/home.js
@@ -9,12 +9,19 @@ const refs = {
   };
 
 
+// Добавляем слушатели событий на Книжки и "See Mor" при загрузке страницы
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('click', clickSeeMore);
+    document.addEventListener('click', clickByBook);
+    document.addEventListener('click', clickByAllCategory);
+});
+
+// Ждем загрузки страницы и создаём список лучших книг
 document.addEventListener("DOMContentLoaded", async function() {
     refs.topListElem.innerHTML = '';
     const topListData = getTopList();
     const topList = await topListData;
     topList.forEach(elem => {
-        // console.log(elem);
         renderTopList(elem)
     });
 });
@@ -46,32 +53,41 @@ function renderBooks(books) {
     return bookHtml;
 }
 
-// Слушатель нажатие по книже
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function(event) {
-        if (event.target.parentElement?.classList.contains('top_list-book_cover_wrapper')) {
-            const bookId = event.target.parentElement.dataset.bookid;
-            console.log(bookId);
-            onOpenModal(bookId);
-        }
-    });
-});
 
-// Слушатель кнопки "See More"
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function(event) {
-        if (event.target.classList.contains('top_list-see_more')) {
-            const category = event.target.dataset.category;
-            titleCategory(category)
-            categoryList(category);
-        }
-    });
-});
+// Клика по Книге
+const clickByBook = function(event) {
+    if (event.target.parentElement?.classList.contains('top_list-book_cover_wrapper')) {
+        const bookId = event.target.parentElement.dataset.bookid;
+        onOpenModal(bookId);
+    }
+};
+
+// Клик по кнопке "See More"
+const clickSeeMore = function(event) {
+    if (event.target.classList.contains('top_list-see_more')) {
+        refs.topListElem.classList.add('hidden');
+        refs.categoryListElem.classList.remove('hidden');
+        const category = event.target.dataset.category;
+        titleCategory(category);
+        categoryList(category);
+        document.removeEventListener('click', clickByBook); // удаляем созданные слуштаели событий по книгам
+        document.addEventListener('click', clickByBook); // создаем новые только для тех которые только что отрендерели
+    }
+};
+
+// Клика по кнопке All Categories
+const clickByAllCategory = async function(event) {
+    if (event.target.classList.contains('all_categories')) {
+        refs.titleElement.innerHTML = 'Best Sellers <span>Books</span>'; 
+        refs.categoryListElem.classList.add('hidden');                
+        refs.topListElem.classList.remove('hidden');
+    }
+};
 
 
 // Асинхронная функция которая получает список книг по категории и отрисовывает его
 async function categoryList(category) {
-    topList.remove();
+    refs.categoryListElem.innerHTML = '';
     const categoryDataPromise = getBooksByCategory(category);
     const categoryData = await categoryDataPromise;
     categoryData.forEach(book => {
@@ -84,10 +100,13 @@ async function categoryList(category) {
             <h3 class="top_list-book_title">${book.title}</h3>
             <p class="top_list-book_author">${book.author}</p>
         </li>
-    `;
-    refs.categoryListElem.innerHTML += html;
+        `;
+        refs.categoryListElem.innerHTML += html;
     });
+    const btnAllCategoriesHTML = `<li><button class="all_categories">All Categories</button></li>`;
+    refs.categoryListElem.innerHTML += btnAllCategoriesHTML;
 };
+
 
 // функция меняет название категории вверху
 function titleCategory(category) {
