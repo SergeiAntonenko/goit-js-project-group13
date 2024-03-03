4ff986d5b5a019eba0082e45c9d2b402135683f2
SergeiAntonenko <gerbar50@gmail.com>
2024-03-03 19:20:29 +0200

    1

--- src/js/support-ukr.js
+++ src/js/support-ukr.js
@@ -1,4 +1,4 @@
-import children from '../img/support-ukr/save-the-children.png';
+import children from 'src/img/support-ukr/save-the-children.png';
 import children2x from './img/support-ukr/save-the-children@2x.png';
 import hope from './img/support-ukr/project-hope.png';
 import hope2x from './img/support-ukr/project-hope@2x.png';
