6d3732b00838b30ac8727c1f70d864cc51ff94e1
H-Nika <88vr07@gmail.com>
2024-03-04 23:20:27 +0200

    changes mob-menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,29 +1,13 @@
-const mobMenu = document.querySelector('.header-menu');
+
   
 mobMenu.addEventListener('click', mobMenuOpen);
 
-function mobMenuOpen() {
-  mobileMenu.classList.toggle('is-open');
+document.addEventListener('DOMContentLoaded', function () {
+    const mobMenu = document.querySelector('.header-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
 
-      if (mobMenu.classList.contains('none')) {
-      mobMenu.classList.add('flex');
-  }
-  return
-}
-
-const homePageElem = document.querySelector('.mob-menu-link');
-const shoppingListElem = document.querySelector('.mob-menu-list-link');
-
-homePageElem.addEventListener('click', function() {
-    homePageElem.classList.remove('mob-menu-link');
-    shoppingListElem.classList.remove('mob-menu-list-link');
-    
-    this.classList.add('mob-menu-link');
+    mobMenu.addEventListener('click', function () {
+        mobileMenu.classList.toggle('.is-open');
+    });
 });
 
-shoppingListElem.addEventListener('click', function() {
-    homeElement.classList.remove('mob-menu-link');
-    shoppingListElem.classList.remove('mob-menu-list-link');
-    
-    this.classList.add('mob-menu-list-link');
-});
