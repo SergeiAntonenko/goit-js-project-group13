9e96ef4d3e0e4f283aa33b5015ebdd1f929373ca
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-03-05 00:54:25 +0400

    warnings

---
+++ src/warnings.js
@@ -0,0 +1,20 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+export function showNoBooksToast() {
+  iziToast.error({
+    title: 'Warning',
+    message: 'There are no more books in this category',
+    position: 'topRight',
+    backgroundColor: '#eac645',
+  });
+}
+
+export function showNotInLibraryToast() {
+  iziToast.error({
+    title: 'Warning',
+    message: 'There are no more books in this library',
+    position: 'topRight',
+    backgroundColor: '#eac645',
+  });
+}
