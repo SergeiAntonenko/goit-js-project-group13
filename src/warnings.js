aad2f1f9c28dd68534acb47e023d10114da84afc
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-03-05 08:33:50 +0400

    ok

--- src/warnings.js
+++ src/warnings.js
@@ -13,7 +13,7 @@ export function showNoBooksToast() {
 export function showNotInLibraryToast() {
   iziToast.error({
     title: 'Warning',
-    message: 'There are no more books in this library',
+    message: 'There are no more categories in this library',
     position: 'topRight',
     backgroundColor: '#eac645',
   });
