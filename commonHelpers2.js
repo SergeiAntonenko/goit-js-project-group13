97761f99f07b4cbff7dd20309194820386716866
SergeiAntonenko <110186592+SergeiAntonenko@users.noreply.github.com>
2024-03-05 07:05:38 +0000

    Deploying to gh-pages from @ SergeiAntonenko/goit-js-project-group13@a91c68f1224693f5897bedf9ea3458bbd06bc72d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,18 +1,18 @@
-import{f as c,a as f,h as b,c as k,r as y}from"./assets/mob-menu-c76d503d.js";import{P as L}from"./assets/vendor-372b0e6c.js";import"https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";const B="/goit-js-project-group13/assets/trash-03-478df87d.svg",P="/goit-js-project-group13/assets/modal-icons-6da17b7d.svg#icon-amazon-logo",$="/goit-js-project-group13/assets/modal-icons-6da17b7d.svg#icon-ibooks-logo",a={bookList:document.querySelector(".shopping-list-saved"),emptyList:document.querySelector(".shopping-list-empty"),pagination:document.querySelector(".tui-pagination")},g=window.innerWidth>=768?3:4;m();async function m(){const e=c();if(f(a.bookList),!e||e.length===0)p(a.emptyList),j(a.pagination);else{const t=await b(e);if(t&&t.length!==0){const i=S(t,g),s=o.getCurrentPage();I(i[s-1]),p(a.pagination),document.querySelectorAll(".saved-item-delete-btn").forEach(r=>r.addEventListener("click",w))}}k()}function A(e){return e.map(({_id:t,book_image:i,title:s,list_name:r,description:u,author:v,buy_links:h})=>{const d=h.reduce((l,n)=>(n.name==="Amazon"&&(l.amazon=n.url),n.name==="Apple Books"&&(l.apple=n.url),l),{});return`<li class="saved-item">
+import{f as l,a as k,h as y,c as L,r as $}from"./assets/mob-menu-fbfe79de.js";import{P}from"./assets/vendor-372b0e6c.js";import"https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";const B="/goit-js-project-group13/assets/trash-03-478df87d.svg",j="/goit-js-project-group13/assets/modal-icons-6da17b7d.svg#icon-amazon-logo",w="/goit-js-project-group13/assets/modal-icons-6da17b7d.svg#icon-ibooks-logo",p="/goit-js-project-group13/assets/icons-shop-5d5cc664.svg",i={bookList:document.querySelector(".shopping-list-saved"),emptyList:document.querySelector(".shopping-list-empty"),pagination:document.querySelector(".tui-pagination")},m=window.innerWidth>=768?3:4,I=window.innerWidth>=768?3:2;v();async function v(){const e=l();if(g(i.pagination),k(i.bookList),!e||e.length===0)u(i.emptyList),g(i.pagination);else{const s=await y(e);if(s&&s.length!==0){const a=z(s,m),t=o.getCurrentPage();H(a[t-1]),u(i.pagination),document.querySelectorAll(".saved-item-delete-btn").forEach(r=>r.addEventListener("click",M))}}L()}function A(e){return e.map(({_id:s,book_image:a,title:t,list_name:r,description:h,author:f,buy_links:b})=>{const d=b.reduce((c,n)=>(n.name==="Amazon"&&(c.amazon=n.url),n.name==="Apple Books"&&(c.apple=n.url),c),{});return`<li class="saved-item">
       <div class="saved-item-cover">
         <img
           class="saved-item-cover-img"
-          src="${i}"
-          alt="${s}"
+          src="${a}"
+          alt="${t}"
         />
       </div>
       <div class="saved-item-discription">
         <div class="saved-item-discription-top">
           <div class="saved-item-title-wrap">
-            <h2 class="saved-item-title">${s}</h2>
+            <h2 class="saved-item-title">${t}</h2>
             <p class="saved-item-genre">${r}</p>
           </div>
-          <button class="saved-item-delete-btn" data-id=${t}>
+          <button class="saved-item-delete-btn" data-id=${s}>
             <img
               class="delete-btn-img"
               src= ${B}
@@ -21,10 +21,10 @@ import{f as c,a as f,h as b,c as k,r as y}from"./assets/mob-menu-c76d503d.js";im
           </button>
         </div>
         <p class="saved-item-short">
-          ${u}
+          ${h}
         </p>
         <div class="saved-item-wrap-bottom">
-          <p class="saved-item-author">${v}</p>
+          <p class="saved-item-author">${f}</p>
           <ul class="saved-item-refer-list">
             <li class="saved-item-refer-item">
               <a
@@ -37,7 +37,7 @@ import{f as c,a as f,h as b,c as k,r as y}from"./assets/mob-menu-c76d503d.js";im
                   width="62"
                   height="19"
                 >
-                  <use href=${P}></use>
+                  <use href=${j}></use>
                 </svg>
               </a>
             </li>
@@ -52,12 +52,16 @@ import{f as c,a as f,h as b,c as k,r as y}from"./assets/mob-menu-c76d503d.js";im
                   width="16"
                   height="16"
                 >
-                  <use href=${$}></use>
+                  <use href=${w}></use>
                 </svg>
               </a>
             </li>
           </ul>
         </div>
       </div>
-    </li>`}).join("")}function I(e){const t=A(e);a.bookList.insertAdjacentHTML("beforeend",t)}function j(e){e.classList.add("is-hidden")}function p(e){e.classList.remove("is-hidden")}function w(e){const t=e.currentTarget.dataset.id;y(t),a.bookList.innerHTML="",o.setTotalItems(c().length),o.movePageTo(o.getCurrentPage())}const H=document.getElementById("pagination"),M={totalItems:c().length,itemsPerPage:g,visiblePages:2,page:1,centerAlign:!0,firstItemClassName:"tui-first-child",lastItemClassName:"tui-last-child",template:{page:'<a href="#" class="tui-page-btn">{{page}}</a>',currentPage:'<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',moveButton:'<a href="#" class="tui-page-btn tui-{{type}}"><span class="tui-ico-{{type}}">{{type}}</span></a>',disabledMoveButton:'<span class="tui-page-btn tui-is-disabled tui-{{type}}"><span class="tui-ico-{{type}}">{{type}}</span></span>',moreButton:'<a href="#" class="tui-page-btn tui-{{type}}-is-ellip"><span class="tui-ico-ellip">...</span></a>'}},o=new L(H,M);o.on("afterMove",({page:e})=>{a.bookList.innerHTML="",m()});function S(e,t){let i=[];for(let s=0;s<e.length;s+=t)i.push(e.slice(s,s+t));return i}
+    </li>`}).join("")}function H(e){const s=A(e);i.bookList.insertAdjacentHTML("beforeend",s)}function g(e){e.classList.add("is-hidden")}function u(e){e.classList.remove("is-hidden")}function M(e){const s=e.currentTarget.dataset.id;$(s),i.bookList.innerHTML="",o.setTotalItems(l().length),o.movePageTo(o.getCurrentPage())}const S=document.getElementById("pagination"),T={totalItems:l().length,itemsPerPage:m,visiblePages:I,page:1,centerAlign:!0,firstItemClassName:"tui-first-child",lastItemClassName:"tui-last-child",template:{page:'<a href="#" class="tui-page-btn">{{page}}</a>',currentPage:'<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',moveButton:`<a href="#" class="tui-page-btn tui-{{type}}"><span class="tui-ico-{{type}}"><svg >
+                    <use href = "${p}#icon-pagination-{{type}}"></use>
+                </svg></span></a>`,disabledMoveButton:`<span class="tui-page-btn tui-is-disabled tui-{{type}}"><span class="tui-ico-{{type}}"><svg >
+                    <use href = "${p}#icon-pagination-{{type}}"></use>
+                </svg></span></span>`,moreButton:'<a href="#" class="tui-page-btn tui-{{type}}-is-ellip"><span class="tui-ico-ellip">...</span></a>'}},o=new P(S,T);o.on("afterMove",({page:e})=>{i.bookList.innerHTML="",v()});function z(e,s){let a=[];for(let t=0;t<e.length;t+=s)a.push(e.slice(t,t+s));return a}
 //# sourceMappingURL=commonHelpers2.js.map
