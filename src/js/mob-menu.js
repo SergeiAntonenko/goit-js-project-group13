5563a107ade5b49972a050247dea131c716535f5
SergeiAntonenko <gerbar50@gmail.com>
2024-03-06 22:53:33 +0200

    upd mob menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,3 +1,4 @@
+const html = document.getElementsByTagName('html')[0];
 const mobMenu = document.querySelector('.header-menu');
 const mobMenuClose = document.querySelector('.header-menu-close');
 const mobileMenu = document.querySelector('.mobile');
@@ -10,10 +11,12 @@ mobMenu.addEventListener('click', function () {
   mobileMenu.classList.toggle('is-open');
   mobMenu.classList.add('disabled');
   mobMenuClose.classList.remove('disabled');
+  html.classList.add('no-scroll');
 });
 
 mobMenuClose.addEventListener('click', function () {
   mobileMenu.classList.toggle('is-open');
   mobMenuClose.classList.add('disabled');
   mobMenu.classList.remove('disabled');
+  html.classList.remove('no-scroll');
 });
