06cf713cafc0ceb74868db406465e2fbfc96821a
SergeiAntonenko <gerbar50@gmail.com>
2024-03-07 11:43:05 +0200

    upd signup

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,12 +2,14 @@ const html = document.getElementsByTagName('html')[0];
 const mobMenu = document.querySelector('.header-menu');
 const mobMenuClose = document.querySelector('.header-menu-close');
 const mobileMenu = document.querySelector('.mobile');
+const signupBtn = document.querySelector('.header-btn');
 
 mobMenu.addEventListener('click', function () {
   mobileMenu.classList.toggle('is-open');
   mobMenu.classList.add('disabled');
   mobMenuClose.classList.remove('disabled');
   html.classList.add('no-scroll');
+  signupBtn.classList.add('signup-is-active');
 });
 
 mobMenuClose.addEventListener('click', function () {
@@ -15,4 +17,5 @@ mobMenuClose.addEventListener('click', function () {
   mobMenuClose.classList.add('disabled');
   mobMenu.classList.remove('disabled');
   html.classList.remove('no-scroll');
+  signupBtn.classList.remove('signup-is-active');
 });
