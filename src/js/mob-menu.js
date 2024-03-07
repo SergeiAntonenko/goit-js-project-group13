b1515d5f3e05fd44fb92bf975162347e2be35338
SergeiAntonenko <gerbar50@gmail.com>
2024-03-07 11:59:53 +0200

    upd home ls btn

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,10 +1,16 @@
+const page = window.location.href;
+const currentPage = page.split('/').pop();
+
 const html = document.getElementsByTagName('html')[0];
 const mobMenu = document.querySelector('.header-menu');
 const mobMenuClose = document.querySelector('.header-menu-close');
 const mobileMenu = document.querySelector('.mobile');
 const signupBtn = document.querySelector('.header-btn');
+const homeBtn = document.querySelector('.mob-menu-link');
+const shoppingListBtn = document.querySelector('.mob-menu-list-link');
 
 mobMenu.addEventListener('click', function () {
+  getCurrentPageMob();
   mobileMenu.classList.toggle('is-open');
   mobMenu.classList.add('disabled');
   mobMenuClose.classList.remove('disabled');
@@ -19,3 +25,13 @@ mobMenuClose.addEventListener('click', function () {
   html.classList.remove('no-scroll');
   signupBtn.classList.remove('signup-is-active');
 });
+
+function getCurrentPageMob() {
+  if (currentPage === 'shopping-list.html') {
+    homeBtn.classList.remove('active');
+    shoppingListBtn.classList.add('active');
+  } else {
+    shoppingListBtn.classList.remove('active');
+    homeBtn.classList.add('active');
+  }
+}
